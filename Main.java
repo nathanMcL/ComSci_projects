ceb2440bec0150983a0a169c6df68080d28225fe
Nathan McLaughlin <129904249+nathanMcL@users.noreply.github.com>
2023-05-31 14:05:42 -0700

    Add files via upload
    Permutation Combination

--- Main.java
+++ Main.java
@@ -1,11 +1,29 @@
-package clock;
+package permutationCombination;
 
-public class Main {
+public class Main 
+{
 
-	public static void main(String[] args) {
-		
-		new MyFrame();
+	public static void main(String[] args) 
+	{
+	int n = 15, r = 4, comb, per;
+	per = factorial(n) / factorial(n-r);
+	System.out.println("Permutation: " + per);
+	comb = factorial(n) / (factorial(r) * factorial(n-r));
+	System.out.println("Combination: " + comb);
+				
+	}
 
+		static int factorial(int n)
+		{
+			int fact =1;
+			int i = 1;
+			while(i <= n)
+			{
+				fact *= i;
+				i++;
+			}
+			return fact;
+			
 	}
 
 }
