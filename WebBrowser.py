3f666872be91b575dff93beae7331e9ad12959f5
Nathan McLaughlin <129904249+nathanMcL@users.noreply.github.com>
2023-06-09 04:09:50 -0700

    web browser
    See comments in code. couldn't resolve PyQt5 QWebEngineView, removed red squiggles but still INOP.

---
+++ WebBrowser.py
@@ -0,0 +1,83 @@
+#this web browser program PyQt5 is inop. Would like to resolve.
+# Have searched solutions, resolved all red squiggles. still now browser
+
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+import PyQt5.QtWebEngineWidgets
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtPrintSupport import *
+import os
+import sys
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.browser = PyQt5.QWebEngineView()
+        self.browser.setUrl(QUrl("http://google.com"))
+        self.browser.urlChanged.connect(self.update_urlbar)
+        self.browser.loadFinished.connect(self.update_title)
+        self.setCentralWidget(self.browser)
+        self.browser = QWebEngineView()
+        self.status = QStatusBar()
+        self.setStatusBar(self.status)
+
+        navtb = QToolBar("Navigation")
+        self.addToolBar(navtb)
+
+        back_btn = QAction("Back", self)
+        back_btn.setStatusTip("Back to previous page")
+        back_btn.triggered.connect(self.browser.back)
+        navtb.addAction(back_btn)
+
+        next_btn = QAction("Forward", self)
+        next_btn.setStatusTip("Forward to the next page")
+        next_btn.triggered.connect(self.browser.forward)
+        navtb.addAction(next_btn)
+
+        reload_btn = QAction("Reload", self)
+        reload_btn.setStatusTip("Reload page")
+        reload_btn.triggered.connect(self.browser.reload)
+        navtb.addAction(reload_btn)
+
+        home_btn = QAction("Home", self)
+        home_btn.setStatusTip("Go Home")
+        home_btn.triggered.connect(self.navigation_home)
+        navtb.addAction(home_btn)
+
+        navtb.addSeparator()
+
+        self.urlbar = QLineEdit()
+        self.urlbar.returnPressed.connect(self.navigate_to_url)
+        navtb.addWidget(self.urlbar)
+
+        stop_btn = QAction("Stop", self)
+        stop_btn.setStatusTip("Stop loading current page")
+        stop_btn.triggered.connect(self.browser.stop)
+        navtb.addAction(stop_btn)
+
+        self.show()
+
+    def update_title(self):
+        title = self.browser.page().title()
+        self.setWindowTitle("%s - Nate's Browser" % title)
+
+    def navigation_home(self):
+        self.browser.setUrl(QUrl("http://www.google.com"))
+
+    def navigate_to_url(self):
+        q = QUrl(self.urlbar.text())
+        if q.scheme() == "":
+            q.setScheme("http")
+        self.browser.setUrl(q)
+
+    def update_urlbar(self, q):
+        self.urlbar.setText(q.toString())
+        self.urlbar.setCursorPosition(0)
+
+
+app = QApplication(sys.argv)
+app.setApplicationName("Nate's Browser")
+window = MainWindow()
+sys.exit(app.exec_())
