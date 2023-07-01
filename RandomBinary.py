97465c5b7be55e494a6d1f92bfb83a3951baf594
Nathan McLaughlin <129904249+nathanMcL@users.noreply.github.com>
2023-07-01 16:13:24 -0700

    Update RandomBinary.py
    # This program generates a range of random binary numbers with the decimal equivalent
    # Prompts user to enter a binary number
    # If inputted binary number is equal to one of the random binary decimal equivalents, a message is printed

--- RandomBinary.py
+++ RandomBinary.py
@@ -1,3 +1,9 @@
+# This program generates a range of random binary numbers with the decimal equivalent
+# Prompts user to enter a binary number
+# If inputted binary number is equal to one of the random binary decimal equivalents, a message is printed
+
+
+
 import random
 
 
@@ -18,6 +24,7 @@ def RRAC():
     print()
 
 
+# Set Binary Range
 for i in range(20):
     RRAC()
 
@@ -28,9 +35,14 @@ def UBI():
     binary_list = list(binary_number)
     decimal_number = 0
     for i in range(len(binary_list)):
-        decimal_number += int(binary_list[i]) * 2 ** (len(binary_list) - i - 1)
-    print("The decimal equivalent of the binary number is", decimal_number)
+        if binary_list[i] != '':
+            decimal_number += int(binary_list[i]) * 2 ** (len(binary_list) - i - 1)
+    if decimal_number == decimal_number:
+        print("Hello!", decimal_number)
+    else:
+        print("The decimal equivalent of the binary number is", decimal_number)
 
 
+# Main
 if __name__ == "__main__":
     UBI()
