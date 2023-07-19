bbaf8c12172a09f200777b44ae321b7cf421b567
Nathan McLaughlin <129904249+nathanMcL@users.noreply.github.com>
2023-07-19 16:39:01 -0700

    Update Socket_Test.py
    # The purpose of this program is to communicate with a client
    # To communicate with client input client IP address
    # Send a message to a client...eventually

--- Socket_Test.py
+++ Socket_Test.py
@@ -1,12 +1,19 @@
+# The purpose of this program is to communicate with a client
 # To communicate with client input client IP address
-
+# Send a message to a client...eventually
 import socket
 
-server = socket.socket()
+# Create a socket at server side
+server = socket.socket(socket.AF_INET,
+                       socket.SOCK_STREAM)
 print('Socket Created')
 
+# Bind the socket with server
+# and port number
 server.bind(('localhost', 9999))
 
+# Allow maximum connection: 3
+# to the socket
 server.listen(3)
 print('waiting for connections')
 
@@ -16,5 +23,10 @@ while True:
     print("Connected with", address, name)
 
     client.send(bytes('Welcome, I am MacN, Your Admin', 'utf-8'))
+
+    # Send a message to a client
+    #client.send(b"what is your status?")
+    #msg = "Status check"
+    #client.send(msg.encode())
 
     client.close()
