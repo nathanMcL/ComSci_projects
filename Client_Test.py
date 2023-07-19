2ebc0507a69683cc8d5f268649ab8d20ec877f44
Nathan McLaughlin <129904249+nathanMcL@users.noreply.github.com>
2023-07-19 16:42:31 -0700

    Update Client_Test.py
    # To communicate with the Socket_Test remote server
    # With remote server Client needs server IP address
    # Send / Receive message ...eventually

--- Client_Test.py
+++ Client_Test.py
@@ -1,12 +1,21 @@
+# To communicate with the Socket_Test remote server
 # With remote server Client needs server IP address
+# Send / Recieve message...eventually 
 
 import socket
 
-client = socket.socket()
+# create a socket at client side
+# using TCP / IP protocol
+client = socket.socket(socket.AF_INET,
+                       socket.SOCK_STREAM)
 
+# connect it to server and port
+# number on a local computer.
 client.connect(('localhost', 9999))
 
+# Client enters name
 name = input("Enter your Name")
+# Client name is sent
 client.send(bytes(name, 'utf-8'))
 
 print(client.recv(1024).decode())
