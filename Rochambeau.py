c87b382400aed95f45caefee5460379eb727e690
Nathan McLaughlin <129904249+nathanMcL@users.noreply.github.com>
2023-09-04 06:25:17 -0700

    Update Rochambeau.py
    # This program plays a game of Rock, Paper, Scissors
    # between the user and the computer.
    # Prompts to play again.
    # Display score at the end of the game.

--- Rochambeau.py
+++ Rochambeau.py
@@ -1,65 +1,65 @@
 # This program plays a game of Rock, Paper, Scissors
 # between the user and the computer.
+# Prompts to play again.
+# Display score at the end of the game.
 
 import random
 
+# Initialize scores
+# Declare global variables so that they can be accessed by all functions
+user_wins = 0  # Number of times the user has won
+computer_wins = 0  # Number of times the computer has won
+ties = 0  # Number of ties
 
-# This function gets the user's choice.
+
 def get_user_choice():
     """
     Get the user's choice of rock, paper, or scissors.
-
-    Raises:
-        ValueError: If the user's choice is invalid.
-
-    Returns:
-        str: The user's choice.
     """
+
     print("Enter your choice: rock, paper, or scissors")
     user_choice = input().lower()
-    if user_choice not in ["rock", "paper", "scissors"]:
-        raise ValueError("Invalid choice. Please enter rock, paper, or scissors.")
+    # Validate the user's choice
+    while user_choice not in ['rock', 'paper', 'scissors']:
+        print("Invalid choice. Please enter rock, paper, or scissors.")
+        user_choice = input().lower()
     return user_choice
 
 
-# This function gets the computer's choice.
 def get_computer_choice():
     """
     Get the computer's choice of rock, paper, or scissors.
-
-    Returns:
-        str: The computer's choice.
     """
-    return random.choice(["rock", "paper", "scissors"])
+
+    # Use the random library to generate a random choice
+    return random.choice(['rock', 'paper', 'scissors'])
 
 
-# This function determines the winner of the game.
 def determine_winner(user, computer):
     """
-    Determines the winner of the game based on the user's choice and the computer's choice.
-
-    Args:
-        user (str): The user's choice.
-        computer (str): The computer's choice.
-
-    Returns:
-        str: A message indicating the winner of the game.
+    Return the winner of the game.
     """
+
+    global user_wins, computer_wins, ties
+    # Check for a tie
     if user == computer:
-        return "It's a tie!"
-    elif (user == "rock" and computer == "scissors") or \
-       (user == "scissors" and computer == "paper") or \
-       (user == "paper" and computer == "rock"):
-        return "You Win!!!"
+        ties += 1
+        return 'It\'s a tie!'
+    # Check for a win
+    elif (user == 'rock' and computer == 'scissors') or \
+            (user == 'scissors' and computer == 'paper') or \
+            (user == 'paper' and computer == 'rock'):
+        user_wins += 1
+        return 'You Win!!!'
     else:
-        return "The Computer Wins!"
+        computer_wins += 1
+        return 'The Computer Wins!'
 
 
-# This is the main function.
 if __name__ == "__main__":
     print("Welcome to Rochambeau!")
 
-    # Play the game until the user quits.
+    # Play the game until the user quits
     while True:
         user_choice = get_user_choice()
         computer_choice = get_computer_choice()
@@ -67,8 +67,9 @@ if __name__ == "__main__":
         print(f"Computer chose {computer_choice}.")
         print(determine_winner(user_choice, computer_choice))
 
-        # Ask if the player wants to play again.
-        play_again = input("would you like to play again? (yes/no)").lower()
+        # Ask if the player wants to play again
+        play_again = input("Would you like to play again? (yes/no): ").lower()
         if play_again != 'yes':
+            print(f"Game Score: You - {user_wins}, Computer - {computer_wins}, Ties - {ties}")
             print("Thank you for playing!")
             break
